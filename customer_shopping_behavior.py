ed2f422472af7d0348abeda0056db9bdcbda64d7
ShayanIsrar <shayanisrar19@gmail.com>
2026-03-29 23:23:12 -0700

    Add files via upload

---
+++ customer_shopping_behavior.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+df = pd.read_csv('customer_shopping_behavior.csv')
+
+# print(df.head())
+
+# print(df.info())
+
+# print(df.describe(include='all'))
+
+
+df['Review Rating'] = df.groupby('Category')['Review Rating'].transform(lambda x: x.fillna(x.median()))
+
+# print(df.isnull().sum())
+
+df.columns = df.columns.str.lower()
+df.columns = df.columns.str.replace(' ','_')
+
+df = df.rename(columns={'purchase_amount_(usd)':'purchase_amount'})
+# print(df.columns)
+
+#create a new column age_group
+
+labels = ['Young Adult', 'Adult', 'Middle-aged', 'Senior']
+df['age_group'] = pd.qcut(df['age'], q=4, labels=labels)
+
+# print(df[['age','age_group']].head(10))
+
+# create column purchase_frequency_days
+
+frequency_mapping = {'Fortnightly':14, 'Weekly':7,'Monthly':30,'Quarterly':90,'Bi-Weekly':14,'Annually':365,'Every 3 Months':90}
+
+df['purchase_frequency_days'] = df['frequency_of_purchases'].map(frequency_mapping)
+
+# print(df[['purchase_frequency_days','frequency_of_purchases']].head(10))
+
+# print(df[['discount_applied','promo_code_used']].head(10))
+
+
+# print((df['discount_applied'] == df['promo_code_used']).all())
+
+df = df.drop('promo_code_used', axis=1)
+
+# print(df.columns)
+
+# PostgreSQL connection
+engine = create_engine("postgresql://postgres:gre321@localhost:5432/customer_behavior")
+
+
+# Upload data to PostgreSQL
+df.to_sql('customer', engine, if_exists='replace', index=False)
+
+print("Data uploaded successfully 🚀")
